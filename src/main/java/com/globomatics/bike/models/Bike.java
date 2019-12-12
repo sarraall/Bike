ecda4715f1aa462c3844032103e32daa5655a2c0
sarraall <allaguisarra15@gmail.com>
2019-12-12 15:07:03 +0100

    First commit

---
+++ src/main/java/com/globomatics/bike/models/Bike.java
@@ -0,0 +1,85 @@
+package com.globomatics.bike.models;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity(name="bike")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Bike {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long bike_id;
+	private String name;
+	private String email;
+	private String phone;
+	private String model;
+	private BigDecimal purchasePrice;
+	@JsonFormat(shape= JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy")
+	private Date purchaseDate;
+	private boolean contact;
+	
+	
+	
+	
+
+	public Long getBike_id() {
+		return bike_id;
+	}
+	public void setBike_id(Long bike_id) {
+		this.bike_id = bike_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public BigDecimal getPurchasePrice() {
+		return purchasePrice;
+	}
+	public void setPurchasePrice(BigDecimal purchasePrice) {
+		this.purchasePrice = purchasePrice;
+	}
+	public Date getPurchaseDate() {
+		return purchaseDate;
+	}
+	public void setPurchaseDate(Date purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+	public boolean isContact() {
+		return contact;
+	}
+	public void setContact(boolean contact) {
+		this.contact = contact;
+	}
+	
+	
+
+}
